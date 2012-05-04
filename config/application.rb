24226bdae456812a1e7472f6c9f4fcb9582610bd
Ben Kempner <ben@verticalbrands.com>
2012-05-04 13:55:51 -0700

    remove coffescript, use haml

--- config/application.rb
+++ config/application.rb
@@ -45,6 +45,10 @@ module ListyStory
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
 
+    config.generators do |g|
+      g.template_engine :haml
+    end
+
     # Use SQL instead of Active Record's schema dumper when creating the database.
     # This is necessary if your schema can't be completely dumped by the schema dumper,
     # like if you have constraints or database-specific column types
