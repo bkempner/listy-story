11d87e2afbf80b3fb40ef3146224870690f5ffa0
Ben Kempner <ben@verticalbrands.com>
2012-05-04 13:33:35 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ListyStory::Application.config.secret_token = 'ee301864031376dffb0ee3b8aa4fdaad6c625d0aa9690188959d688b7b1ac127e96be6c072dee227914a8add00ec08b5cdc2effcdbc4c9e1f79e1d9dd90a10b2'
