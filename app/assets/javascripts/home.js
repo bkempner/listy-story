2eed6e85303634cefb111377054686e2b5bedc78
Bordicon <pandazero@gmail.com>
2012-05-04 18:53:58 -0700

    moved inital styling to scss file, cleaned up js

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -15,29 +15,10 @@ $(document).ready(function() {
   
   scrollorama.animate('#fade-in',{ delay: 400, duration: 700, property:'opacity', start:0 });
   			
-  // animate some easing examples
-  var $easing = $('#easing'),
-    $clone = $easing.clone().appendTo('#examples-easing')
-      .css({position:'relative',top:'-2.95em'})
-      .lettering();
-  $easing.css({ color: '#131420', textShadow: '0 1px 0 #363959' });
-  easing_array = ['easeOutBounce',
-                  'easeOutQuad',
-                  'easeOutCubic',
-                  'easeOutQuart',
-                  'easeOutQuint', 
-                  'easeOutExpo'];
-  $clone.find('span')
-    .each( function( idx, el ){
-    scrollorama.animate( $(this), {	delay:400, duration: 500, 
-    property:'top', end: 300,
-    easing: easing_array[idx] });
-  })
-  // initialize the plugin, pass in the class selector for the sections of content (blocks)
   var scrollorama = $.scrollorama({ blocks:'.scrollblock' });
 			
-  // assign function to add behavior for onBlockChange event
-  scrollorama.onBlockChange(function() {
-    console.log('hi');
-  });
+  // // assign function to add behavior for onBlockChange event
+  // scrollorama.onBlockChange(function() {
+  //   console.log('hi');
+  // });
 });
