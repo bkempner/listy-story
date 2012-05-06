c659a8619a1a82b33e4b13e6b35b0e9e8ac8499f
Ben Kempner <ben@verticalbrands.com>
2012-05-06 03:12:46 -0700

    opening panel

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -12,10 +12,14 @@ $(document).ready(function() {
       .css('display','block')
       .text('onBlockChange | blockIndex:'+i+' | current block: '+scrollorama.settings.blocks.eq(i).attr('id'));
   });
-  
-  scrollorama.animate('#fade-in',{ delay: 400, duration: 700, property:'opacity', start:0 });
+ 
+  scrollorama.animate('#opening-pane .subtitle',{ delay: 0, duration: 250, start:0, property: 'opacity' });
+  scrollorama.animate('#opening-pane #popout',{ delay: 250, duration: 0, start:0, property: 'opacity' });
+  scrollorama.animate('#listy',{ delay: 300, duration: 500, property:'padding-top', start:200 });
+  scrollorama.animate('#opening-pane .callout',{ delay: 900, duration: 50, start:0, property: 'opacity' });
+  scrollorama.animate('#opening-pane .end-panel',{ delay: 0, duration: 1000, start:0, property: 'padding-top', pin: true });
   			
-  var scrollorama = $.scrollorama({ blocks:'.scrollblock' });
+  //scrollorama.animate('#unpin',{ duration:500, property:'padding-top', start:400, pin:true });
 			
   // // assign function to add behavior for onBlockChange event
   // scrollorama.onBlockChange(function() {
